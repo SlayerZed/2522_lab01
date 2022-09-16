555f763ce669f61c53492a5c36a262595df4b083
Raghav Soni <rsoni14@my.bcit.ca>
2022-09-16 12:15:52 -0700

    added greeting

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("my animal is tiger");
+        System.out.println("Is a leaf's only purpose to fall?");
     }
 }
