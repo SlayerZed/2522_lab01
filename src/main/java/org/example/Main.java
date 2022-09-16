5360d37fb1cc79cd9eb0aa402a2b1146b2d1058d
Raghav Soni <rsoni14@my.bcit.ca>
2022-09-16 12:18:13 -0700

    added greeting

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+/**
+ * just added my fav quote as a greeting
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Is a leaf's only purpose to fall?");
